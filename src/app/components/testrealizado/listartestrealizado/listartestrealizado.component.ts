d6eb1c8d9e6f147aae01278ae5c2b4a9a41cd7dd
RenzoDPV <renzopearl23@gmail.com>
2025-06-18 04:45:21 -0500

    B Listar comentarios y listar test realizados
    u	angular.json
    
    :

--- src/app/components/testrealizado/listartestrealizado/listartestrealizado.component.ts
+++ src/app/components/testrealizado/listartestrealizado/listartestrealizado.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Testrealizado } from '../../../models/testrealizado';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { TestrealizadoService } from '../../../services/testrealizado.service';
 
 @Component({
   selector: 'app-listartestrealizado',
-  imports: [],
+  imports: [MatTableModule, MatButtonModule, MatIconModule],
   templateUrl: './listartestrealizado.component.html',
   styleUrl: './listartestrealizado.component.css'
 })
-export class ListartestrealizadoComponent {
+export class ListartestrealizadoComponent implements OnInit{
+  displayedColumns: string[] = ['c1','c2','c3','c4','c5'];
+  dataSource:MatTableDataSource<Testrealizado>=new MatTableDataSource()
+  constructor(private trS:TestrealizadoService){}
 
+  ngOnInit(): void {
+      this.trS.list().subscribe(data=>{
+        this.dataSource=new MatTableDataSource(data)
+      })
+  }
 }
--- src/app/components/testrealizado/testrealizado.component.ts
+++ src/app/components/testrealizado/testrealizado.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ListartestrealizadoComponent } from "./listartestrealizado/listartestrealizado.component";
 
 @Component({
   selector: 'app-testrealizado',
-  imports: [],
+  imports: [RouterOutlet, ListartestrealizadoComponent],
   templateUrl: './testrealizado.component.html',
   styleUrl: './testrealizado.component.css'
 })
 export class TestrealizadoComponent {
-
+  constructor(public route:ActivatedRoute){}
 }
